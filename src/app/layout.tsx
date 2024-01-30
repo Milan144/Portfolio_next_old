dc670bca9fb313990d7010a4ce05e7ab1196d4fe
nokk <milan.hommet@protonmail.com>
2024-01-30 19:03:06 +0100

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
-import Background from "./components/Background"
-import Navbar from "./components/Navbar"
+import Background from "./background"
+import Navbar from "./navbar"
 
 export const metadata: Metadata = {
   title: "Milan Hommet",
